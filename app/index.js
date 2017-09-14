eb86cdbe1afba83d9079757cb324ec9d4060d47b
hellojackhui <HUIJIAWEI@hotmail.com>
2017-09-14 21:58:27 +0800

    2017-9-15

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {AppContainer} from 'react-hot-loader';
-import Root from './root'
+import Root from './root';
 
 render(
     <AppContainer>
--- webpack.config.js
+++ webpack.config.js
@@ -12,46 +12,45 @@ module.exports={
 		path.join(__dirname,'app/index.js')
 	],
 	output: {
-        path:path.join(__dirname,'/dist/'),
+        path: path.join(__dirname, '/dist/'),
         filename: '[name].js',
-		publicPath:'/'
+        publicPath: '/'
     },
     plugins: [
-    	//渲染html模板自动生成新的html
-    	new HtmlWebpackPlugin({
-			template:'./index.tpl.html',
-			inject:'body',
-			filename:'./index.html'
-		}),
-		new webpack.optimize.OccurrenceOrderPlugin(),
-		new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV':JSON.stringify('development')
-		})
-	],
+        new HtmlWebpackPlugin({
+          template: './index.tpl.html',
+          inject: 'body',
+          filename: './index.html'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
     module: {
-		loaders:[
-			{
-				//所有以js为尾的文件
-				test:/\.js$/,
-				//exclude:哪些文件夹不需要处理
-				//node_modules:node_modules下用npm装的依赖库不需处理
-				exclude:/node_modules/,
-				loaders:"babel-loader",
-				query:
-					{
-						presets:['react','es2015']
-					}
-			},
-					{
-				test:/\.css$/,
-				loaders:"style!css"
-					},
-					{
-                test:/\.less/,
-                loaders:'style-loader!css-loader!less-loader'
-					}
-		]
-	}
-}
+        resolve:{
+            extensions:['','.js','.json']
+        },        
+        loaders: [
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: "babel-loader",
+              query:
+                {
+                  presets:['react','es2015']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            {
+                test: /\.less/,
+                loader: 'style-loader!css-loader!less-loader'
+            }
+        ]
+    }
+};
